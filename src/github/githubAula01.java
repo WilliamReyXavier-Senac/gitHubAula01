e8243f9656ffdc5915e07e2f8a405bb45c32c003
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-08-26 21:44:20 -0300

    Adicionado outro prinln

--- src/github/githubAula01.java
+++ src/github/githubAula01.java
@@ -5,7 +5,7 @@ public class githubAula01 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Welcome to GitHub? :D");
-        
+        System.out.println("Printagem 3");
         
         
     }
